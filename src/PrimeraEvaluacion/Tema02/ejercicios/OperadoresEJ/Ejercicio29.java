b934dafc91bc36507f9da565cbf1ec5ca0449f9e
Kamilly-cod <kamillybritodm@gmail.com>
2023-10-04 19:27:36 +0200

    Nuevos ejercicios1

--- src/PrimeraEvaluacion/Tema02/ejercicios/OperadoresEJ/Ejercicio29.java
+++ src/PrimeraEvaluacion/Tema02/ejercicios/OperadoresEJ/Ejercicio29.java
@@ -1,11 +1,18 @@
 package PrimeraEvaluacion.Tema02.ejercicios.OperadoresEJ;
 /*
-Hacer el ejercicio
+Escribe un programa que lea tres números y compruebe que todos son diferentes, es decir, no iguales entre sí. El programa debe imprimir un booleano (true o false).
  */
 
 
-
+import java.util.Scanner;
 
 public class Ejercicio29 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int sorte = teclado.nextInt();
+        int sorte1 = teclado.nextInt();
+        int sorte3 = teclado.nextInt();
+
+    }
 
 }
